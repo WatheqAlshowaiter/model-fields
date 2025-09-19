eda9bca40c387e72faa80217ef6855801d4c28b1
WatheqAlshowaiter <24838274+WatheqAlshowaiter@users.noreply.github.com>
2025-09-19 23:44:53 +0000

    Fix styling

--- tests/ModelFieldsCommandTest.php
+++ tests/ModelFieldsCommandTest.php
@@ -16,14 +16,13 @@ class ModelFieldsCommandTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * @param  array  $params
-     *
      * @return string
      */
     private function runCommandAndGetOutput(array $params)
     {
-        $buffer = new BufferedOutput();
+        $buffer = new BufferedOutput;
         Artisan::call('model:fields', $params, $buffer);
+
         return trim($buffer->fetch());
     }
 
@@ -252,7 +251,8 @@ class ModelFieldsCommandTest extends TestCase
         Cache::forget('model-fields.banner_shown');
 
         // Create a subclass that overrides openUrl()
-        $stubCommand = new class extends ModelFieldsCommand {
+        $stubCommand = new class extends ModelFieldsCommand
+        {
             public string $calledWith = '';
 
             // Change to protected so test can inspect
@@ -264,7 +264,7 @@ class ModelFieldsCommandTest extends TestCase
         };
 
         // Replace the command in Laravel's container so artisan uses our stub
-        $this->app->extend(ModelFieldsCommand::class, fn() => $stubCommand);
+        $this->app->extend(ModelFieldsCommand::class, fn () => $stubCommand);
 
         $this->artisan('model:fields', [
             'model' => Father::class,
