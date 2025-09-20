6abf34425962b28842e0e5e2c6dbe3e376a7877b
WatheqAlshowaiter <24838274+WatheqAlshowaiter@users.noreply.github.com>
2025-09-20 10:01:13 +0000

    Fix styling

--- tests/ModelFieldsCommandTest.php
+++ tests/ModelFieldsCommandTest.php
@@ -240,7 +240,8 @@ class ModelFieldsCommandTest extends TestCase
         Cache::forget('model-fields.banner_shown');
 
         // Create a subclass that overrides openUrl()
-        $stubCommand = new class extends ModelFieldsCommand {
+        $stubCommand = new class extends ModelFieldsCommand
+        {
             public string $calledWith = '';
 
             // Change to protected so test can inspect
@@ -252,7 +253,7 @@ class ModelFieldsCommandTest extends TestCase
         };
 
         // Replace the command in Laravel's container so artisan uses our stub
-        $this->app->extend(ModelFieldsCommand::class, fn() => $stubCommand);
+        $this->app->extend(ModelFieldsCommand::class, fn () => $stubCommand);
 
         $this->artisan('model:fields', [
             'model' => Father::class,
