3143fbd6b2bef9102354e1861cea9fdf0d93651a
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2025-11-08 00:10:20 +0300

    chore: fix typos

--- tests/database/migrations/2025_11_07_100247_create_uncles_table.php
+++ tests/database/migrations/2025_11_07_100247_create_uncles_table.php
@@ -11,9 +11,9 @@ class CreateUnclesTable extends Migration
     public function up(): void
     {
         Schema::create('uncles', function (Blueprint $table) {
-            $table->string('boot_creating'); // filed by creating boot method in model
-            $table->string('boot_saving'); //filed by saving boot method in model
-            $table->string('observer_creating'); // filed by observer creating
+            $table->string('boot_creating'); // filled by creating boot method in model
+            $table->string('boot_saving'); // filled by saving boot method in model
+            $table->string('observer_creating'); // filled by observer creating
             $table->string('observer_saving'); // filled by observer saving
             $table->string('event_creating'); // filled by event creating
             $table->string('event_saving'); // filled by event saving
